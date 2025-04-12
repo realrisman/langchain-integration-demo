d27cbca6977e33d97c086e278b1c69ba878c6a74
Muhamad Risman <real.risman@gmail.com>
2025-04-12 21:26:34 +0700

    refactor: update Home component exports to include ImplementationSteps and remove unused exports for improved modularity and maintainability

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,6 +1,4 @@
 export * from "./hero-section";
 export * from "./features-section";
+export * from "./implementation-steps";
 export * from "./tech-stack-section";
-export * from "./tech-stack-item";
-export * from "./feature-card";
-export * from "./footer";
