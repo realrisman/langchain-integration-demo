f4796dca2b46907e02f564cfc9432a199260c1f5
Muhamad Risman <real.risman@gmail.com>
2025-04-12 14:50:56 +0700

    feat: create Home component index file to export all subcomponents for better modularization and organization

---
+++ src/components/home/index.ts
@@ -0,0 +1,6 @@
+export * from "./hero-section";
+export * from "./features-section";
+export * from "./tech-stack-section";
+export * from "./tech-stack-item";
+export * from "./feature-card";
+export * from "./footer";
