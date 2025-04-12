46a522bb651f4020d301a4005b98d0daf179832b
Muhamad Risman <real.risman@gmail.com>
2025-04-12 23:15:14 +0700

    refactor: remove unused inputRef prop from MealPlannerPage component to streamline code and improve maintainability

--- src/app/(app)/meal-planner/page.tsx
+++ src/app/(app)/meal-planner/page.tsx
@@ -68,7 +68,6 @@ export default function MealPlannerPage() {
               setInputValue={setInputValue}
               handleSubmit={handleSubmit}
               isLoading={isLoading}
-              inputRef={inputRef}
             />
           </div>
         </div>
